6df8a0fa55b625758d05044a6a74b43f8da3b16d
Alejandro Silva <a.s.cebreros@gmail.com>
2021-04-04 22:17:18 -0500

    pruebas

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,8 +3,7 @@ const User = require('../models/User.model')
 const {validationResult} = require('express-validator')
 
 exports.allOrders = async (req, res) => {
-  const {_id} = usuario._id
-  const orders = await Order.find({user: _id}).sort({createdAt: -1}).populate('user')
+  const orders = await Order.find({user: usuario._id}).sort({createdAt: -1}).populate('user')
   res.json({orders})
 }
 
